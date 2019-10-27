9edddc846f523e6fa188c3c278731c881cfe85f8
Luke Thompson <endeavour9@gmail.com>
2019-10-27 19:14:30 +1100

    add a mac script to build an installer https://openjdk.java.net/jeps/343 https://github.com/mirkosertic/FXDesktopSearch https://medium.com/@adam_carroll/java-packager-with-jdk11-31b3d620f4a8

---
+++ package.sh
@@ -0,0 +1,30 @@
+JDK_14_VERSION=openjdk-14-jpackage+1-49_osx-x64_bin.tar.gz
+JDK_14_EXTRACTED_PATH=jdk-14.jdk
+
+rm -rf ./${JDK_14_EXTRACTED_PATH}
+rm -rf ./${JDK_14_VERSION}
+
+wget https://download.java.net/java/early_access/jpackage/1/${JDK_14_VERSION}
+tar xzf ${JDK_14_VERSION}
+export JPACKAGE_HOME=./${JDK_14_EXTRACTED_PATH}
+export PATH=$JAVA_HOME/bin:$PATH
+export MAVEN_OPTS=-"Xmx3072m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit"
+
+mvn clean package
+
+${JPACKAGE_HOME}/Contents/Home/bin/jpackage --package-type pkg \
+    -d ./target/jpackage-app \
+    -n WereKitten \
+    --description "A game where you control a cat and go on deadly quests." \
+    --resource-dir ./src/main/deploy/jpackage \
+    -i ./target/jpackage-app \
+    --icon ./src/main/resources/icons/app.ico \
+    --main-class com.mycodefu.start.Start \
+    --main-jar WereKitten.jar \
+    --app-version 1.0 \
+    --license-file ./LICENSE \
+    --temp ./target/jpackage \
+    --runtime-image ${JAVA_HOME} \
+    --copyright "2019 Jett Thompson" \
+    --vendor "Jett Thompson" \
+    --verbose
