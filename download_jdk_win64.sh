7f1e72ad67611bf92dd72b87743eb43d296d3746
Luke Thompson <endeavour9@gmail.com>
2019-11-02 12:19:58 +1100

    update the download location for jpackage

--- download_jdk_win64.sh
+++ download_jdk_win64.sh
@@ -1,10 +1,9 @@
-JDK_14_VERSION=openjdk-14-jpackage+1-49_windows-x64_bin.zip
+JDK_14_VERSION=openjdk-14-jpackage+1-64_windows-x64_bin.zip
 JDK_14_EXTRACTED_PATH=jdk-14
 
 if [[ ! -e $JDK_14_EXTRACTED_PATH ]]; then
   rm -rf ./${JDK_14_EXTRACTED_PATH}
   rm -rf ./${JDK_14_VERSION}
-
   wget https://download.java.net/java/early_access/jpackage/1/${JDK_14_VERSION}
   unzip ${JDK_14_VERSION}
 
