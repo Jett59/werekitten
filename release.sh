6b377b3117b956802bd64a32dee17decaa612251
Luke Thompson <endeavour9@gmail.com>
2019-11-10 20:52:52 +1100

    update the release script to only push the tag you are working on

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@ echo "v0.0.$1" >> RELEASES.md
 git add RELEASES.md
 git commit -m "release v0.0.$1"
 git tag -a "v0.0.$1" -m "release v0.0.$1"
-git push origin --tags
+git push origin "v0.0.$1"
 git push
