fcd7aee14a4d0c1f3ec71b91ff72402e65ebff5f
Luke Thompson <endeavour9@gmail.com>
2019-11-10 20:35:31 +1100

    add idea for getting CIRCLE_TAG broken in 2.0

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 echo "v0.0.$1" >> README.md
-git add .
+git add README.md
 git commit -m "release v0.0.$1"
 git tag -a "v0.0.$1" -m "release v0.0.$1"
-git push origin "v0.0.$1"
+git push origin --tags
 git push
