28fbdd4f7a0f9a7ffea275e7a48424061e2cbd00
Luke Thompson <endeavour9@gmail.com>
2019-12-08 19:32:29 +1000

    log when the resource stream returned is null

--- src/main/java/com/mycodefu/werekitten/image/ImageHelper.java
+++ src/main/java/com/mycodefu/werekitten/image/ImageHelper.java
@@ -9,17 +9,23 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 public class ImageHelper {
-	public static BufferedImage readBufferedImage(String resourceFilePath) {
-		InputStream in = ImageHelper.class.getResourceAsStream(resourceFilePath);
-			BufferedImage img;
-			try {
-				img = ImageIO.read(in);
-			} catch (IOException e) {
-				throw new IllegalArgumentException("could not load the image: "+resourceFilePath+", original exception: "+e.getMessage());
-			}
-			return img;
-	}
-	public static Image readFxImage(String resourcePath) {
-		return SwingFXUtils.toFXImage(readBufferedImage(resourcePath), null);
-	}
+    public static BufferedImage readBufferedImage(String resourceFilePath) {
+        InputStream in = ImageHelper.class.getResourceAsStream(resourceFilePath);
+        if (in == null) {
+            String message = String.format("Could not load the image: %s, getResourceAsStream returned null.", resourceFilePath);
+            System.out.println(message);
+            throw new IllegalArgumentException(message);
+        }
+        BufferedImage img;
+        try {
+            img = ImageIO.read(in);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("could not load the image: " + resourceFilePath + ", original exception: " + e.getMessage());
+        }
+        return img;
+    }
+
+    public static Image readFxImage(String resourcePath) {
+        return SwingFXUtils.toFXImage(readBufferedImage(resourcePath), null);
+    }
 }
