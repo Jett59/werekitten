6186776cdef6222d8544603e54b2e1bec1ec22cc
Jett Thompson <jett.thompson.59@gmail.com>
2020-01-03 08:10:33 +1100

    fixed a bug with multiplayer

--- src/main/java/com/mycodefu/werekitten/application/GameLoop.java
+++ src/main/java/com/mycodefu/werekitten/application/GameLoop.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import com.mycodefu.werekitten.event.NetworkEventType;
 import com.mycodefu.werekitten.event.PlayerEventType;
+import com.mycodefu.werekitten.player.Player;
 import com.mycodefu.werekitten.ui.UI;
 
 import javafx.animation.AnimationTimer;
@@ -25,8 +26,8 @@ public class GameLoop extends AnimationTimer {
     public void handle(long now) {
         //run the event handling 10 times using a for loop
         for (int n = 0; n < 5; n++) {
-            runPlayerEvents(player1EventQueue);
-            runPlayerEvents(player2EventQueue);
+            runPlayerEvents(player1EventQueue, ui.getPlayer1());
+            runPlayerEvents(player2EventQueue, ui.getPlayer2());
         }
 
         for (int n = 0; n < 5; n++) {
@@ -56,7 +57,7 @@ public class GameLoop extends AnimationTimer {
     }
 
     //use the events to run methods
-    private void runPlayerEvents(Queue<PlayerEventType> eventQueue) {
+    private void runPlayerEvents(Queue<PlayerEventType> eventQueue, Player target) {
         //get the playerEventType from the front of the playerEventqueue
         PlayerEventType playerEventType = eventQueue.poll();
         //check if the event is null, if it is, that means that the queue is empty
@@ -67,32 +68,27 @@ public class GameLoop extends AnimationTimer {
         //run operations based on the event's type, using a switch statement
         switch (playerEventType) {
             case moveLeft: {
-                //run the moveLeft method on the UI
-                ui.getPlayer1().moveLeft(CAT_MOVE_AMOUNT);
+                target.moveLeft(CAT_MOVE_AMOUNT);
                 break;
             }
 
             case moveRight: {
-                //run the moveRight method on the UI
-                ui.getPlayer1().moveRight(CAT_MOVE_AMOUNT);
+                target.moveRight(CAT_MOVE_AMOUNT);
                 break;
             }
 
             case jump: {
-                //run the jump method on the UI
-                ui.getPlayer1().jump();
+                target.jump();
                 break;
             }
 
             case stopMovingLeft: {
-                //run the stopMovingLeft method on the UI
-                ui.getPlayer1().stopMovingLeft();
+                target.stopMovingLeft();
                 break;
             }
 
             case stopMovingRight: {
-                //run the stopMovingRight method on the UI
-                ui.getPlayer1().stopMovingRight();
+                target.stopMovingRight();
                 break;
             }
 
