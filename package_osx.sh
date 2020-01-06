ac34f3e23993b2b189a9f710edb78c91d9dea967
Luke Thompson <endeavour9@gmail.com>
2020-01-06 17:37:09 +1100

    small script updates

--- package_osx.sh
+++ package_osx.sh
@@ -8,7 +8,7 @@ export JAVA_HOME="${JPACKAGE_HOME}/Contents/Home"
 export MAVEN_HOME=$(cat MAVEN_HOME.txt)
 
 export MAVEN_OPTS=-"Xmx3072m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit"
-#$MAVEN_HOME/bin/mvn clean package
+$MAVEN_HOME/bin/mvn clean package
 
 ${JPACKAGE_HOME}/Contents/Home/bin/jpackage \
       --name WereKitten \
@@ -23,18 +23,3 @@ ${JPACKAGE_HOME}/Contents/Home/bin/jpackage \
       --module-path ./target/release-directory/lib \
       --add-modules javafx.controls \
       --verbose
-#      --resource-dir ./src/main/deploy/jpackage \
-#      --runtime-image ./amazon-corretto-11.jdk/Contents/Home \
-
-#${JPACKAGE_HOME}/Contents/Home/bin/jpackage \
-#      --name WereKitten \
-#      --module-path ./jdk-14.jdk/Contents/Home/jmods:target/release-directory/lib:target/release-directory/WereKitten.jar \
-#      -m com.mycodefu.werekitten \
-#      --verbose
-#      --runtime-image ./amazon-corretto-11.jdk/Contents/Home \
-
-#${JPACKAGE_HOME}/Contents/Home/bin/jpackage \
-#    -d ./target/jpackage-app \
-#    -n WereKitten \
-#    --module-path $JAVA_HOME/jmods:target/jpackage-app/lib:target/jpackage-app/WereKitten.jar \
-#    -m com.mycodefu.werekitten
