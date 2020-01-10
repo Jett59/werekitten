54a12b5c05505b61d97fdc58601be951b370b781
Jett Thompson <jett.thompson.59@gmail.com>
2020-01-10 16:16:40 +1100

    readded the window

--- src/main/java/com/mycodefu/werekitten/pipeline/handlers/game/UICreationHandler.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/handlers/game/UICreationHandler.java
@@ -3,10 +3,30 @@ package com.mycodefu.werekitten.pipeline.handlers.game;
 import com.mycodefu.werekitten.pipeline.PipelineContext;
 import com.mycodefu.werekitten.pipeline.PipelineEvent;
 import com.mycodefu.werekitten.pipeline.handlers.PipelineHandler;
+import com.mycodefu.werekitten.ui.GameUI;
 
 public class UICreationHandler implements PipelineHandler {
     @Override
     public void handleEvent(PipelineContext context, PipelineEvent event) {
-        System.out.println("Starting game!!");
+    	if(event.getPipelineName().equalsIgnoreCase("game")) {
+    		switch (event.getEvent().getName()) {
+    		case "start": {
+    			context.getStage().show();
+    			context.getStage().setScene(
+    					new GameUI()
+    					.getScene(1024, 768)
+    					);
+    			break;
+    		}
+    		
+    		case "quit": {
+    			context.getStage().hide();
+    			break;
+    		}
+    		
+    		default:
+    			throw new IllegalArgumentException("gameEventType "+event.getEvent().getName()+" could not be determined");
+    		}
+    	}
     }
 }
