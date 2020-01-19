6be11ccf649e58a84d52b44b291aed790aa5d29c
Luke Thompson <endeavour9@gmail.com>
2020-01-20 08:49:08 +1100

    remove unused imports

--- src/main/java/com/mycodefu/werekitten/pipeline/events/game/GameEvent.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/events/game/GameEvent.java
@@ -1,8 +1,5 @@
 package com.mycodefu.werekitten.pipeline.events.game;
 
-import com.mycodefu.werekitten.event.Event;
-import com.mycodefu.werekitten.event.GameEventType;
-import com.mycodefu.werekitten.event.PlayerEventType;
 import com.mycodefu.werekitten.pipeline.PipelineEvent;
 
 public abstract class GameEvent implements PipelineEvent {
