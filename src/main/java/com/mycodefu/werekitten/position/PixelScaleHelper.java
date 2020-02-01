4325be9e0ec8b04b0bb0a697314c1e4b7bf67f63
Jett Thompson <jett.thompson.59@gmail.com>
2020-02-02 09:46:20 +1100

    added the pixelScaleHelper class

---
+++ src/main/java/com/mycodefu/werekitten/position/PixelScaleHelper.java
@@ -0,0 +1,25 @@
+package com.mycodefu.werekitten.position;
+
+public class PixelScaleHelper {
+private int scaleWidth, scaleHeight;
+private int screenWidth, screenHeight;
+
+public PixelScaleHelper(int scaleWidth, int scaleHeight, int screenWidth, int screenHeight) {
+	this.scaleHeight = scaleHeight;
+	this.scaleWidth = scaleWidth;
+	this.screenHeight = screenHeight;
+	this.screenWidth = screenWidth;
+}
+
+public int scaleX(int x) {
+	double ratio = (double)x/(double)scaleWidth;
+	int result = (int) (ratio*screenWidth);
+	return result;
+}
+
+public int scaleY(int y) {
+	double ratio = (double)y/(double)scaleHeight;
+	int result = (int) (ratio*screenHeight);
+	return result;
+}
+}
---
+++ src/test/java/com/mycodefu/werekitten/location/PixelScaleHelperTest.java
@@ -0,0 +1,18 @@
+package com.mycodefu.werekitten.location;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.mycodefu.werekitten.position.PixelScaleHelper;
+
+
+public class PixelScaleHelperTest {
+
+@Test
+void scale_x_Test() {
+	PixelScaleHelper pixelScaleHelper = new PixelScaleHelper(200, 200, 1000, 1000);
+	assertEquals(100, pixelScaleHelper.scaleX(20));
+	assertEquals(500, pixelScaleHelper.scaleX(100));
+}
+}
