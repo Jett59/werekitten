63f75214450b7e8d6b3e31ec4ec8123f1171ad46
Luke Thompson <endeavour9@gmail.com>
2020-02-23 11:34:46 +1100

    remove the double-scaling of the x value in the UiNetworkEventHandler

--- src/main/java/com/mycodefu/werekitten/pipeline/handlers/Ui/UiNetworkEventHandler.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/handlers/Ui/UiNetworkEventHandler.java
@@ -1,8 +1,5 @@
 package com.mycodefu.werekitten.pipeline.handlers.Ui;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 import com.mycodefu.werekitten.event.UiEventType;
 import com.mycodefu.werekitten.level.PixelScaleHelper;
 import com.mycodefu.werekitten.pipeline.PipelineContext;
@@ -12,70 +9,73 @@ import com.mycodefu.werekitten.pipeline.handlers.PipelineHandler;
 import com.mycodefu.werekitten.player.Player;
 import com.mycodefu.werekitten.ui.UI;
 
-public class UiNetworkEventHandler implements PipelineHandler {
-	private UI ui;
-	private List<PipelineEvent> unusedNetworkEvents = new CopyOnWriteArrayList<>();
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-	@Override
+public class UiNetworkEventHandler implements PipelineHandler {
+    private UI ui;
+    private List<PipelineEvent> unusedNetworkEvents = new CopyOnWriteArrayList<>();
+
+    @Override
     public void handleEvent(PipelineContext context, PipelineEvent event) {
         if (event.getPipelineName().equalsIgnoreCase("Ui")) {
-			PixelScaleHelper pixelScaleHelper = context.level().get() != null ? context.level().get().getPixelScaleHelper() : null;
-			switch ((UiEventType)event.getEvent()) {
-				case UiCreated: {
-					this.ui = ((UiCreatedEvent) event).getUI();
-					break;
-				}
-				case networkMoveLeft:{
-					Player player = context.getPlayerMap().get(((NetworkMoveLeftEvent)event).getPlayerId());
-					player.moveLeftTo(pixelScaleHelper.scaleX(((NetworkMoveLeftEvent)event).x));
-					break;
-				}
-				case networkMoveRight:{
-					Player player = context.getPlayerMap().get(((NetworkMoveRightEvent)event).getPlayerId());
-					player.moveRightTo(pixelScaleHelper.scaleX(((NetworkMoveRightEvent)event).x));
-					break;
-				}
-				case networkStopMovingLeft:{
-					Player player = context.getPlayerMap().get(((NetworkStopMovingLeftEvent)event).getPlayerId());
-					player.stopMovingLeft();
-					break;
-				}
-				case networkStopMovingRight:{
-					Player player = context.getPlayerMap().get(((NetworkStopMovingRightEvent)event).getPlayerId());
-					player.stopMovingRight();
-					break;
-				}
-				case networkJump: {
-					NetworkJumpEvent jumpEvent = (NetworkJumpEvent)event;
-					if(checkPlayerIsValidAndExecuteEvents(jumpEvent.getPlayerId(), context)) {
-						Player player = context.getPlayerMap().get(jumpEvent.getPlayerId());
-						player.jump();
-					}else {
-						unusedNetworkEvents.add(event);
-					}
-					break;
-				}
-				case networkServerListening: {
-					ui.setAddress(((NetworkServerListeningEvent)event).getAddress());
-					break;
-				}
+            PixelScaleHelper pixelScaleHelper = context.level().get() != null ? context.level().get().getPixelScaleHelper() : null;
+            switch ((UiEventType) event.getEvent()) {
+                case UiCreated: {
+                    this.ui = ((UiCreatedEvent) event).getUI();
+                    break;
+                }
+                case networkMoveLeft: {
+                    Player player = context.getPlayerMap().get(((NetworkMoveLeftEvent) event).getPlayerId());
+                    player.moveLeftTo(((NetworkMoveLeftEvent) event).x);
+                    break;
+                }
+                case networkMoveRight: {
+                    Player player = context.getPlayerMap().get(((NetworkMoveRightEvent) event).getPlayerId());
+                    player.moveRightTo(((NetworkMoveRightEvent) event).x);
+                    break;
+                }
+                case networkStopMovingLeft: {
+                    Player player = context.getPlayerMap().get(((NetworkStopMovingLeftEvent) event).getPlayerId());
+                    player.stopMovingLeft();
+                    break;
+                }
+                case networkStopMovingRight: {
+                    Player player = context.getPlayerMap().get(((NetworkStopMovingRightEvent) event).getPlayerId());
+                    player.stopMovingRight();
+                    break;
+                }
+                case networkJump: {
+                    NetworkJumpEvent jumpEvent = (NetworkJumpEvent) event;
+                    if (checkPlayerIsValidAndExecuteEvents(jumpEvent.getPlayerId(), context)) {
+                        Player player = context.getPlayerMap().get(jumpEvent.getPlayerId());
+                        player.jump();
+                    } else {
+                        unusedNetworkEvents.add(event);
+                    }
+                    break;
+                }
+                case networkServerListening: {
+                    ui.setAddress(((NetworkServerListeningEvent) event).getAddress());
+                    break;
+                }
                 default:
                     break;
             }
-        }else {
-        	throw new IllegalArgumentException("the UiNetworkEventHandler may only exist in the UI pipeline, not the pipeline: "+event.getPipelineName());
+        } else {
+            throw new IllegalArgumentException("the UiNetworkEventHandler may only exist in the UI pipeline, not the pipeline: " + event.getPipelineName());
         }
     }
 
-private boolean checkPlayerIsValidAndExecuteEvents(String id, PipelineContext context) {
-	Player player = context.getPlayerMap().get(id);
-	if(player == null) {
-		return false;
-	}else {
-		for(PipelineEvent event: unusedNetworkEvents) {
-			handleEvent(context, event);
-		}
-		return true;
-	}
-}
+    private boolean checkPlayerIsValidAndExecuteEvents(String id, PipelineContext context) {
+        Player player = context.getPlayerMap().get(id);
+        if (player == null) {
+            return false;
+        } else {
+            for (PipelineEvent event : unusedNetworkEvents) {
+                handleEvent(context, event);
+            }
+            return true;
+        }
+    }
 }
