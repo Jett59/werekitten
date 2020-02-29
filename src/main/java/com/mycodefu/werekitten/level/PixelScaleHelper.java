995c617e9f01d99686fb980ed902b256c8317252
Jett Thompson <jett.thompson.59@gmail.com>
2020-02-29 18:37:22 +1100

    optimised the pixelScaleHelper

--- src/main/java/com/mycodefu/werekitten/level/PixelScaleHelper.java
+++ src/main/java/com/mycodefu/werekitten/level/PixelScaleHelper.java
@@ -1,39 +1,26 @@
 package com.mycodefu.werekitten.level;
 
 public class PixelScaleHelper {
-    private double scaleWidth;
-    private double scaleHeight;
-    private double screenWidth;
-    private double screenHeight;
+    private double xMultiplier, yMultiplier;
 
     public PixelScaleHelper(double scaleWidth, double scaleHeight, double screenWidth, double screenHeight) {
-        this.scaleHeight = scaleHeight;
-        this.scaleWidth = scaleWidth;
-        this.screenHeight = screenHeight;
-        this.screenWidth = screenWidth;
+        this.xMultiplier = screenWidth/scaleWidth;
+        this.yMultiplier = screenHeight/scaleHeight;
     }
 
     public double scaleX(double x) {
-        double ratio = x / scaleWidth;
-        double result = ratio * screenWidth;
-        return result;
+        return x*xMultiplier;
     }
 
     public double scaleY(double y) {
-        double ratio = y / scaleHeight;
-        double result = ratio * screenHeight;
-        return result;
+        return y*yMultiplier;
     }
     
     public double scaleXBack(double x) {
-    	double ratio = x / screenWidth;
-    	double result = ratio * scaleWidth;
-    	return result;
+    	return x/xMultiplier;
     }
     
     public double scaleYBack(double y) {
-    	double ratio = y / screenHeight;
-    	double result = ratio * scaleHeight;
-    	return result;
+    	return y/yMultiplier;
     }
 }
