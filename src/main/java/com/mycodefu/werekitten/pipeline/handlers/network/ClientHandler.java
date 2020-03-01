904dea25d21ab7d438986a3986693f54695479f6
Jett Thompson <jett.thompson.59@gmail.com>
2020-03-01 16:03:57 +1100

    removed the clientInformationHandler

--- src/main/java/com/mycodefu/werekitten/pipeline/handlers/network/ClientHandler.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/handlers/network/ClientHandler.java
@@ -2,6 +2,7 @@ package com.mycodefu.werekitten.pipeline.handlers.network;
 
 import com.mycodefu.werekitten.event.Event;
 import com.mycodefu.werekitten.event.NetworkEventType;
+import com.mycodefu.werekitten.event.UiEventType;
 import com.mycodefu.werekitten.netty.client.NettyClient;
 import com.mycodefu.werekitten.netty.client.NettyClientHandler;
 import com.mycodefu.werekitten.network.message.MessageBuilder;
@@ -14,45 +15,54 @@ import com.mycodefu.werekitten.pipeline.events.network.NetworkConnectClientEvent
 import com.mycodefu.werekitten.pipeline.events.network.NetworkEvent;
 import com.mycodefu.werekitten.pipeline.events.ui.NetworkConnectionEstablishedEvent;
 import com.mycodefu.werekitten.pipeline.events.ui.NetworkConnectionEstablishedEvent.ConnectionType;
+import com.mycodefu.werekitten.pipeline.events.ui.UiEvent;
 import com.mycodefu.werekitten.pipeline.handlers.PipelineHandler;
 import com.mycodefu.werekitten.player.NetworkPlayerHelper;
 import io.netty.buffer.ByteBuf;
 
-
-public class ClientHandler implements PipelineHandler, NettyClientHandler.SocketCallback{
+public class ClientHandler implements PipelineHandler, NettyClientHandler.SocketCallback {
 	private final NetworkPlayerHelper networkPlayerHelper;
 	private NettyClient nettyClient;
 	private PipelineContext context;
 
-public ClientHandler() {
-	this.networkPlayerHelper = new NetworkPlayerHelper();
-}
+	public ClientHandler() {
+		this.networkPlayerHelper = new NetworkPlayerHelper();
+	}
+
 	@Override
 	public void handleEvent(PipelineContext context, PipelineEvent event) {
-		if(event.getPipelineName().equalsIgnoreCase("pipeline") && event instanceof NetworkEvent) {
+		if (event instanceof NetworkEvent) {
 			this.context = context;
-			NetworkEvent networkEvent = (NetworkEvent)event;
+			NetworkEvent networkEvent = (NetworkEvent) event;
 			switch (networkEvent.getNetworkEvent()) {
 			case connect: {
-				System.out.println("connect event recieved "+event.toString());
-				nettyClient = new NettyClient(((NetworkConnectClientEvent)event).getServerAddress(), this);
+				System.out.println("connect event recieved " + event.toString());
+				nettyClient = new NettyClient(((NetworkConnectClientEvent) event).getServerAddress(), this);
 				nettyClient.connect();
 				context.postEvent(new BuildLevelEvent(false));
-				context.postEvent(new NetworkConnectionEstablishedEvent(ConnectionType.server, ((NetworkConnectClientEvent)event).getServerAddress()));
+				context.postEvent(new NetworkConnectionEstablishedEvent(ConnectionType.server,
+						((NetworkConnectClientEvent) event).getServerAddress()));
 				break;
 			}
-			case readyForInitMessage: {
+
+			default:
+				break;
+			}
+		} else if (event instanceof UiEvent) {
+			switch ((UiEventType) event.getEvent()) {
+			case UiCreated: {
 				MessageBuilder messageBuilder = MessageBuilder.createNewMessageBuffer(MessageType.init, 3)
-						.addDoubleAsShort(context.level().get().getPixelScaleHelper().scaleXBack(context.level().get().getPlayerElement().getLocation().getX()));
+						.addDoubleAsShort(context.level().get().getPixelScaleHelper()
+								.scaleXBack(context.level().get().getPlayerElement().getLocation().getX()));
 				nettyClient.sendMessage(messageBuilder.getBuffer());
 				break;
 			}
-			
 			default:
 				break;
 			}
 		}
 	}
+
 	@Override
 	public void clientDisconnected(String id) {
 		System.out.println("client destroying player");
@@ -62,23 +72,23 @@ public ClientHandler() {
 
 	@Override
 	public void clientConnected(String id, String remoteAddress) {
-			
+
 	}
 
 	@Override
 	public void clientMessageReceived(String id, ByteBuf content) {
-		networkPlayerHelper.applyNetworkMessageToPlayer(content, id, context, message -> nettyClient.sendMessage(message), false);
+		networkPlayerHelper.applyNetworkMessageToPlayer(content, id, context,
+				message -> nettyClient.sendMessage(message), false);
 	}
+
 	@Override
 	public void clientError(String id, Throwable e) {
-	e.printStackTrace();
+		e.printStackTrace();
 	}
+
 	@Override
 	public Event[] getEventInterest() {
-		return new Event[] {
-				NetworkEventType.readyForInitMessage,
-				NetworkEventType.connect
-		};
+		return new Event[] { UiEventType.UiCreated, NetworkEventType.connect };
 	}
 
 }
