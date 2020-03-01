18ed1015747531c7c346563121bf9b51ee504f44
Luke Thompson <endeavour9@gmail.com>
2020-03-01 15:29:29 +1100

    only send events to handlers that are interested

--- src/main/java/com/mycodefu/werekitten/pipeline/Pipeline.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/Pipeline.java
@@ -1,14 +1,16 @@
 package com.mycodefu.werekitten.pipeline;
 
 import com.mycodefu.werekitten.Start;
+import com.mycodefu.werekitten.event.Event;
+import com.mycodefu.werekitten.event.NetworkEventType;
 import com.mycodefu.werekitten.pipeline.handlers.PipelineHandler;
 
-import java.util.Arrays;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Pipeline {
     private final Queue<PipelineEvent> eventQueue;
+    private final Map<Event, PipelineHandler[]> eventHandlers;
     private final PipelineHandler[] handlers;
     private final int eventsToRunPerFrame;
     private String name;
@@ -20,16 +22,28 @@ public class Pipeline {
         if (handlers == null || handlers.length == 0) {
             throw new IllegalArgumentException("Handlers may not be null or empty.");
         } else {
-            for (int i = 0; i < handlers.length; i++) {
-                if (handlers[i] == null) {
+            Map<Event, List<PipelineHandler>> eventHandlersList = new HashMap<>();
+            for (PipelineHandler handler : handlers) {
+                if (handler == null) {
                     throw new IllegalArgumentException("No handler in the handlers list may be null.");
                 }
+                Event[] handlerEvents = handler.getEventInterest();
+                for (Event handlerEvent : handlerEvents) {
+                    if (!eventHandlersList.containsKey(handlerEvent)) {
+                        eventHandlersList.put(handlerEvent, new ArrayList<>());
+                    }
+                    eventHandlersList.get(handlerEvent).add(handler);
+                }
+            }
+            this.handlers = handlers;
+            this.eventHandlers = new HashMap<>();
+            for (Map.Entry<Event, List<PipelineHandler>> eventListEntry : eventHandlersList.entrySet()) {
+                this.eventHandlers.put(eventListEntry.getKey(), eventListEntry.getValue().toArray(new PipelineHandler[]{}));
             }
         }
         if (eventsToRunPerFrame < 1 || eventsToRunPerFrame > 10) {
             throw new IllegalArgumentException("eventsToRunPerFrame must be between 1 and 10.");
         }
-        this.handlers = handlers;
         this.eventsToRunPerFrame = eventsToRunPerFrame;
         this.eventQueue = new ConcurrentLinkedQueue<>();
     }
@@ -48,12 +62,13 @@ public class Pipeline {
                 return;
             }
             if(Start.DEBUG_PIPELINE_EVENTS) {
-            System.out.println("rendering event "+event.getEvent().getName());
+                System.out.println("rendering event "+event.getEvent().getName());
             }
-
-            for (int i = 0; i < this.handlers.length; i++) {
-                PipelineHandler handler = this.handlers[i];
-                handler.handleEvent(context, event);
+            PipelineHandler[] handlers = this.eventHandlers.get(event.getEvent());
+            if (handlers != null) {
+                for (PipelineHandler handler : handlers) {
+                    handler.handleEvent(context, event);
+                }
             }
         }
     }
@@ -65,7 +80,7 @@ public class Pipeline {
     @Override
     public String toString() {
         return "Pipeline{" +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
                 ", handlers.length=" + handlers.length +
                 '}';
     }
