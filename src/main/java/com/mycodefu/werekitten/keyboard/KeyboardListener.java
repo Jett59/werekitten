43ea974799989afe8984c6f7f99900ef145e6290
Jett Thompson <jett.thompson.59@gmail.com>
2020-03-07 19:24:32 +1100

    atempted to reimplement the serverHandler

---
+++ src/main/java/com/mycodefu/werekitten/keyboard/KeyboardListener.java
@@ -0,0 +1,109 @@
+package com.mycodefu.werekitten.keyboard;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+
+public class KeyboardListener {
+private AnimationTimer timer;
+private Map<KeyCode, KeyType> codeToType = new HashMap<>();
+private KeyboardEventHandler eventHandler;
+private Map<KeyType, AtomicBoolean> typeToDown = new ConcurrentHashMap<>();
+private List<KeyboardEventCallback> keyReleasedCallbackList = new CopyOnWriteArrayList<>();
+private List<KeyboardEventCallback> keyPressedCallbackList = new CopyOnWriteArrayList<>();
+
+public static interface KeyboardEventHandler{
+	void raiseEvent(KeyType type);
+}
+
+public static interface KeyboardEventCallback{
+	void callback(KeyType keyType);
+}
+
+public KeyboardListener(KeyboardEventHandler eventHandler) {
+	this.eventHandler = eventHandler;
+	
+	codeToType.put(KeyCode.LEFT, KeyType.left);
+	codeToType.put(KeyCode.RIGHT, KeyType.right);
+	codeToType.put(KeyCode.SPACE, KeyType.space);
+
+	typeToDown.put(KeyType.left, new AtomicBoolean());
+	typeToDown.put(KeyType.right, new AtomicBoolean());
+	typeToDown.put(KeyType.space, new AtomicBoolean());
+}
+
+public void startListening() {
+	timer = new AnimationTimer() {
+
+		@Override
+		public void handle(long now) {
+			//check if any of the keys in typeToDown are down
+			if(typeToDown.get(KeyType.left).get()) {
+				//raise an event using the eventHandler passed in the constructor
+				eventHandler.raiseEvent(KeyType.left);
+			}
+			if(typeToDown.get(KeyType.right).get()) {
+				//raise an event using the eventHandler passed in the constructor
+				eventHandler.raiseEvent(KeyType.right);
+			}
+			if(typeToDown.get(KeyType.space).get()) {
+				//raise an event using the eventHandler passed in the constructor
+				eventHandler.raiseEvent(KeyType.space);
+			}
+		}
+		
+	};
+	timer.start();
+}
+
+//registers that the key is down
+public void keyPressed(KeyCode code) {
+	//the new value to be set on the map TypeToDown
+	AtomicBoolean newValue = new AtomicBoolean();
+	newValue.set(true);
+	//get the KeyType equivilent of the KeyCode
+	KeyType key = codeToType.get(code);
+	if(key != null) {
+	//replace the value of the KeyType with the newValue
+	typeToDown.replace(key, newValue);
+	//run all of the keyPressedCallbacks, passing the keyType
+	for(KeyboardEventCallback callback : keyPressedCallbackList) {
+		callback.callback(key);
+	}
+	}
+}
+
+//registers that the key is up
+public void keyReleased(KeyCode code) {
+	//the new value to be set on the map TypeToDown
+	AtomicBoolean newValue = new AtomicBoolean();
+	newValue.set(false);
+	//get the KeyType equivilent of the KeyCode
+	KeyType key = codeToType.get(code);
+	if(key != null) {
+	//replace the value of the KeyType with the newValue
+	typeToDown.replace(key, newValue);
+	//run all of the KeyReleasedCallbacks, passing the keyType
+	for(KeyboardEventCallback callback : keyReleasedCallbackList) {
+		callback.callback(key);
+	}
+	}
+}
+
+//add keyPressedCallback
+public void addKeyPressedCallback(KeyboardEventCallback callback) {
+	keyPressedCallbackList.add(callback);
+}
+
+//add keyboardReleasedCallback
+public void addKeyboardReleasedCallback(KeyboardEventCallback callback) {
+	keyReleasedCallbackList.add(callback);
+}
+
+}
--- src/main/java/com/mycodefu/werekitten/pipeline/handlers/network/ServerHandler.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/handlers/network/ServerHandler.java
@@ -2,12 +2,16 @@ package com.mycodefu.werekitten.pipeline.handlers.network;
 
 import com.mycodefu.werekitten.event.Event;
 import com.mycodefu.werekitten.event.NetworkEventType;
+import com.mycodefu.werekitten.event.PlayerEventType;
 import com.mycodefu.werekitten.netty.server.NettyServer;
 import com.mycodefu.werekitten.netty.server.NettyServerHandler;
 import com.mycodefu.werekitten.network.NetworkUtils;
+import com.mycodefu.werekitten.network.message.MessageBuilder;
+import com.mycodefu.werekitten.network.message.MessageType;
 import com.mycodefu.werekitten.pipeline.PipelineContext;
 import com.mycodefu.werekitten.pipeline.PipelineEvent;
 import com.mycodefu.werekitten.pipeline.events.network.NetworkEvent;
+import com.mycodefu.werekitten.pipeline.events.player.PlayerEvent;
 import com.mycodefu.werekitten.pipeline.events.ui.NetworkConnectionEstablishedEvent;
 import com.mycodefu.werekitten.pipeline.events.ui.NetworkServerListeningEvent;
 import com.mycodefu.werekitten.pipeline.handlers.PipelineHandler;
@@ -15,17 +19,21 @@ import com.mycodefu.werekitten.player.NetworkPlayerHelper;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelId;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static com.mycodefu.werekitten.pipeline.events.ui.NetworkConnectionEstablishedEvent.ConnectionType.client;
 
 public class ServerHandler implements PipelineHandler {
     private final NetworkPlayerHelper networkPlayerHelper;
-    NettyServer server;
+    private NettyServer server;
+    private List<ChannelId> channelIds;
 
     public ServerHandler() {
         this.networkPlayerHelper = new NetworkPlayerHelper();
+        this.channelIds = new CopyOnWriteArrayList<>();
     }
 
     @Override
@@ -39,6 +47,7 @@ public class ServerHandler implements PipelineHandler {
 
                         @Override
                         public void serverConnectionOpened(ChannelId id, String remoteAddress) {
+                        	channelIds.add(id);
                             senders.put(id, (playerMessage) -> server.sendMessage(id, playerMessage));
                             context.postEvent(new NetworkConnectionEstablishedEvent(client, remoteAddress));
                         }
@@ -63,6 +72,39 @@ public class ServerHandler implements PipelineHandler {
                     server.close();
                     break;
                 }
+			default:
+				break;
+            }
+        }else if(event instanceof PlayerEvent) {
+        	PlayerEvent playerEvent = (PlayerEvent) event;
+            if (server != null && playerEvent.getPlayerId().equalsIgnoreCase("local")) {
+                switch (playerEvent.getPlayerEvent()) {
+                    case moveLeft:
+                    	for(ChannelId id: channelIds) {
+                        server.sendMessage(id, MessageBuilder.createNewMessageBuffer(MessageType.moveLeft, 1).getBuffer());
+                    	}
+                        break;
+                    case moveRight:
+                    	for(ChannelId id : channelIds) {
+                        server.sendMessage(id, MessageBuilder.createNewMessageBuffer(MessageType.moveRight, 1).getBuffer());
+                    	}
+                        break;
+                    case stopMovingLeft:
+                    	for(ChannelId id : channelIds) {
+                        server.sendMessage(id, MessageBuilder.createNewMessageBuffer(MessageType.idleLeft, 1).getBuffer());
+                    	}
+                        break;
+                    case stopMovingRight:
+                    	for(ChannelId id : channelIds) {
+                        server.sendMessage(id, MessageBuilder.createNewMessageBuffer(MessageType.idleRight, 1).getBuffer());
+                    	}
+                        break;
+                    case jump:
+                    	for(ChannelId id : channelIds) {
+                        server.sendMessage(id, MessageBuilder.createNewMessageBuffer(MessageType.jump, 1).getBuffer());
+                    	}
+                        break;
+                }
             }
         }
     }
@@ -71,7 +113,12 @@ public class ServerHandler implements PipelineHandler {
 	public Event[] getEventInterest() {
 		return new Event[] {
 				NetworkEventType.start,
-				NetworkEventType.stop
+				NetworkEventType.stop,
+				PlayerEventType.moveLeft,
+                PlayerEventType.moveRight,
+                PlayerEventType.stopMovingLeft,
+                PlayerEventType.stopMovingRight,
+                PlayerEventType.jump
 		};
 	}
 
