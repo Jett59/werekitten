ac3d407b0f9af58e691ceacda2984435d4a3d025
lthompson <lthompson@infomedia.com.au>
2020-04-04 18:44:01 +1100

    small improvements to Animation - remove a few calculations during interpolate and go to INDEFINITE animations

--- src/main/java/com/mycodefu/werekitten/animation/MovementAnimation.java
+++ src/main/java/com/mycodefu/werekitten/animation/MovementAnimation.java
@@ -6,38 +6,34 @@ import javafx.animation.Transition;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
-public class MovementAnimation extends Transition{
-	public static final double TARGET_FRAME_RATE = 30d;
+public class MovementAnimation extends Transition {
+    public static final double TARGET_FRAME_RATE = 30d;
 
-private Node node;
-private double x = Double.MAX_VALUE;
-private double pixelRate;
-private Function<Double, Boolean> boundChecker;
+    private Node node;
+    private double x = Double.MAX_VALUE;
+    private double pixelRate;
+    private Function<Double, Boolean> boundChecker;
 
-	@Override
-	protected void interpolate(double frac) {
-		if(x == Double.MAX_VALUE) {
-			x = node.getTranslateX();
-		}
-		
-			if(boundChecker.apply(x+pixelRate)) {
-				x+=pixelRate;
-				node.setTranslateX(x);
-			}
-		}
+    @Override
+    protected void interpolate(double frac) {
+        if (boundChecker.apply(x + pixelRate)) {
+            x += pixelRate;
+            node.setTranslateX(x);
+        }
+    }
 
-public MovementAnimation(Node node, double pixelRate, Function<Double, Boolean> boundChecker, Duration duration) {
-	super(TARGET_FRAME_RATE);
-	this.node = node;
-	this.pixelRate = pixelRate;
-	this.boundChecker = boundChecker;
-	setCycleDuration(duration);
-	setCycleCount(-1);
-}
+    public MovementAnimation(Node node, double pixelRate, Function<Double, Boolean> boundChecker, Duration duration) {
+        super(TARGET_FRAME_RATE);
+        this.node = node;
+        this.pixelRate = pixelRate;
+        this.boundChecker = boundChecker;
+        setCycleDuration(duration);
+        setCycleCount(-1);
+    }
 
-@Override
-	public void play() {
-x = Double.MAX_VALUE;
-		super.play();
-	}
+    @Override
+    public void play() {
+        x = node.getTranslateX();
+        super.play();
+    }
 }
--- src/main/java/com/mycodefu/werekitten/player/Player.java
+++ src/main/java/com/mycodefu/werekitten/player/Player.java
@@ -17,8 +17,8 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Player {
-	public static final double MOTION_PIXEL_RATE = 4d;
-	
+    public static final double MOTION_PIXEL_RATE = 4d;
+
     private final String id;
     private Map<AnimationType, Animation> nameToAnimation = new ConcurrentHashMap<>();
     private List<Animation> animations = new ArrayList<>();
@@ -26,7 +26,7 @@ public class Player {
     private TranslateTransition jump;
     private Animation currentAnimation;
     private double health = 1;
-    private Text healthDisplayText = new Text(""+((int)health)*999);
+    private Text healthDisplayText = new Text("" + ((int) health) * 999);
     private MovementAnimation[] moveTransitions = new MovementAnimation[2];
 
     private static boolean DEBUG = true;
@@ -48,17 +48,14 @@ public class Player {
         jump.setByY(-jumpAmount);
         jump.setAutoReverse(true);
         jump.setCycleCount(2);
-        
+
         int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-MovementAnimation moveRight = new MovementAnimation(animationGroup, MOTION_PIXEL_RATE, x-> {
-	return x < screenWidth-walkRight.getImageView().getViewport().getWidth();
-}, Duration.seconds(1));
+        double rightBound = screenWidth - walkRight.getImageView().getViewport().getWidth();
+        MovementAnimation moveRight = new MovementAnimation(animationGroup, MOTION_PIXEL_RATE, x -> x < rightBound, Duration.INDEFINITE);
         moveTransitions[0] = moveRight;
-        MovementAnimation moveLeft = new MovementAnimation(animationGroup, MOTION_PIXEL_RATE*-1, x-> {
-        	return x > 0;
-        }, Duration.seconds(1));
+        MovementAnimation moveLeft = new MovementAnimation(animationGroup, MOTION_PIXEL_RATE * -1, x -> x > 0, Duration.INDEFINITE);
         moveTransitions[1] = moveLeft;
-        
+
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(initialAnimation));
         animationGroup.setTranslateX(animationGroup.getLayoutX() + initialXPosition);
     }
@@ -68,10 +65,10 @@ MovementAnimation moveRight = new MovementAnimation(animationGroup, MOTION_PIXEL
     }
 
     public void moveLeft(double amount) {
-    	moveTransitions[1].play();
+        moveTransitions[1].play();
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(AnimationType.walkLeft));
 
-        if (DEBUG){
+        if (DEBUG) {
             System.out.println(String.format("Kitten %s moved left by %f.1 to %f.1", id, amount, animationGroup.getLayoutX()));
         }
     }
@@ -80,29 +77,29 @@ MovementAnimation moveRight = new MovementAnimation(animationGroup, MOTION_PIXEL
         moveTransitions[0].play();
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(AnimationType.walkRight));
 
-        if (DEBUG){
+        if (DEBUG) {
             System.out.println(String.format("Kitten %s moved right  by %f.1 to %f.1", id, amount, animationGroup.getLayoutX()));
         }
 
     }
 
-    public void moveLeftTo(double x) {    	
+    public void moveLeftTo(double x) {
         animationGroup.setLayoutX(x);
         //TODO: Check for collisions and undo the move to the rightmost point of the colliding object if so
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(AnimationType.walkLeft));
 
-        if (DEBUG){
+        if (DEBUG) {
             System.out.println(String.format("Kitten %s moved left to %f.1", id, animationGroup.getLayoutX()));
         }
 
     }
 
-    public void moveRightTo(double x) {    	
+    public void moveRightTo(double x) {
         animationGroup.setLayoutX(x);
         //TODO: Check for collisions and undo the move to the leftmost point of the colliding object if so
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(AnimationType.walkRight));
 
-        if (DEBUG){
+        if (DEBUG) {
             System.out.println(String.format("Kitten %s moved right to %f.1", id, animationGroup.getLayoutX()));
         }
     }
@@ -112,17 +109,17 @@ MovementAnimation moveRight = new MovementAnimation(animationGroup, MOTION_PIXEL
     }
 
     public void stopMovingLeft() {
-    	moveTransitions[1].stop();
+        moveTransitions[1].stop();
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(AnimationType.idleLeft));
     }
 
     public void stopMovingRight() {
-    	moveTransitions[0].stop();
+        moveTransitions[0].stop();
         currentAnimation = playOneAnimation(animations, nameToAnimation.get(AnimationType.idleRight));
     }
-    
+
     public Shape getCurrentShape() {
-    	return currentAnimation.getCurrentShape();
+        return currentAnimation.getCurrentShape();
     }
 
     private Animation playOneAnimation(List<Animation> allAnimations, Animation animationToPlay) {
@@ -154,22 +151,22 @@ MovementAnimation moveRight = new MovementAnimation(animationGroup, MOTION_PIXEL
     }
 
     public String getId() {
-    	return id;
+        return id;
     }
-    
-public void dealDamage(double amount) {
-	health-=amount;
-	healthDisplayText.setText(""+(int)(health*999d));
-}
 
-public void recoverHealth(double amount) {
-	health+=amount;
-	healthDisplayText.setText(""+(int)(health*999d));
-}
+    public void dealDamage(double amount) {
+        health -= amount;
+        healthDisplayText.setText("" + (int) (health * 999d));
+    }
 
-public double getHealth() {
-	return health;
-}
+    public void recoverHealth(double amount) {
+        health += amount;
+        healthDisplayText.setText("" + (int) (health * 999d));
+    }
+
+    public double getHealth() {
+        return health;
+    }
 
     @Override
     public String toString() {
