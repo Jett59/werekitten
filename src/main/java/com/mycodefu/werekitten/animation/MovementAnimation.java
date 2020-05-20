7d9a62370eabb9c9e5bf1f09f389f6a40d886bc6
Jett Thompson <jett.thompson.59@gmail.com>
2020-05-20 15:43:18 +1000

    refactored the movement system in an attempt to fix various sync bugs

--- src/main/java/com/mycodefu/werekitten/animation/MovementAnimation.java
+++ src/main/java/com/mycodefu/werekitten/animation/MovementAnimation.java
@@ -1,42 +1,62 @@
 package com.mycodefu.werekitten.animation;
 
+import javafx.animation.Interpolator;
 import javafx.animation.Transition;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
 public class MovementAnimation extends Transition {
-    public static final double TARGET_FRAME_RATE = 60d;
+	public static double pixelRate = 120;
 
     private Node node;
     private double x = Double.MAX_VALUE;
-    private double pixelRate;
     private double rightBound;
     private MovementDirection direction;
-
+    private double distanceToEdge = Double.MAX_VALUE;
+private double lastFrac = 0;
+    
     @Override
     protected void interpolate(double frac) {
-        if (direction == MovementDirection.Left && x - pixelRate >= 0) {
-            x -= pixelRate;
-            node.setTranslateX(x);
-        } else if (direction == MovementDirection.Right && x + pixelRate < rightBound) {
-            x += pixelRate;
-            node.setTranslateX(x);
+        switch (direction) {
+        case Left: {
+        	if(distanceToEdge == Double.MAX_VALUE) {
+        		distanceToEdge = node.getTranslateX();
+        	}
+        	if(frac > lastFrac) {
+        	x = distanceToEdge*(1-frac);
+        	node.setTranslateX(x);
+        	lastFrac = frac;
+        	}
+        	break;
+        }
+		case Right:{
+			if(distanceToEdge == Double.MAX_VALUE) {
+				distanceToEdge = rightBound-node.getTranslateX();
+			}
+			if(frac > lastFrac) {
+				x = rightBound-distanceToEdge*(1-frac);
+				node.setTranslateX(x);
+				lastFrac = frac;
+			}
+		}
+		default:
+			break;
         }
     }
 
     public MovementAnimation(Node node, double pixelRate, MovementDirection initialDirection, double rightBound, Duration duration) {
-        super(TARGET_FRAME_RATE);
         this.node = node;
-        this.pixelRate = pixelRate;
         this.rightBound = rightBound;
         this.direction = initialDirection;
-        setCycleDuration(duration);
-        setCycleCount(-1);
+        setInterpolator(Interpolator.LINEAR);
+        setCycleCount(1);
     }
 
     @Override
     public void play() {
         x = node.getTranslateX();
+        lastFrac = 0;
+        distanceToEdge = Double.MAX_VALUE;
         super.play();
     }
 
@@ -51,6 +71,15 @@ public class MovementAnimation extends Transition {
     
     public void setDirection(MovementDirection direction){
         this.direction = direction;
+        switch (direction) {
+        case Left: {
+        	super.setCycleDuration(Duration.millis((node.getTranslateX()/pixelRate)*1000));
+        	break;
+        }
+        case Right: {
+        	super.setCycleDuration(Duration.millis((rightBound-node.getTranslateX())/pixelRate*1000));
+        }
+        }
     }
 
     public enum MovementDirection {
