c7aca099462fe07905216d70088f4002040d8c13
Jett Thompson <jett.thompson.59@gmail.com>
2020-05-23 17:45:00 +1000

    fixed the teleportation bug

--- src/main/java/com/mycodefu/werekitten/player/Player.java
+++ src/main/java/com/mycodefu/werekitten/player/Player.java
@@ -69,6 +69,7 @@ public class Player {
         if (!Double.isNaN(xSync)) {
             moveTransition.syncX(xSync);
         }
+        moveTransition.stop();
         moveTransition.setDirection(Left);
         if (!moveTransition.getStatus().equals(javafx.animation.Animation.Status.RUNNING)) {
             moveTransition.play();
@@ -84,6 +85,7 @@ public class Player {
         if (!Double.isNaN(xSync)) {
             moveTransition.syncX(xSync);
         }
+        moveTransition.stop();
         moveTransition.setDirection(Right);
         if (!moveTransition.getStatus().equals(javafx.animation.Animation.Status.RUNNING)) {
             moveTransition.play();
