f62321275ea09ae157210dc1cf34d28c3435ce85
Luke Thompson <endeavour9@gmail.com>
2020-06-28 19:35:27 +1000

    add event types for c key

--- src/main/java/com/mycodefu/werekitten/event/KeyboardEventType.java
+++ src/main/java/com/mycodefu/werekitten/event/KeyboardEventType.java
@@ -1,20 +1,21 @@
 package com.mycodefu.werekitten.event;
 
-public enum KeyboardEventType implements Event{
-	F3Pressed,
-	F3Released,
-aPressed,
-aReleased,
-dPressed,
-dReleased,
-spacePressed,
-spaceReleased,
-registerListener,
-unregisterListener
-	;
+public enum KeyboardEventType implements Event {
+    F3Pressed,
+    F3Released,
+    cPressed,
+    cReleased,
+    aPressed,
+    aReleased,
+    dPressed,
+    dReleased,
+    spacePressed,
+    spaceReleased,
+    registerListener,
+    unregisterListener;
 
-@Override
-public String getName() {
-	return toString();
-}
+    @Override
+    public String getName() {
+        return toString();
+    }
 }
