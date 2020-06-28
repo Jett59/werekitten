efdaeca8878d5800d3879bd366073a30a23ae03c
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-28 20:18:06 +1000

    added a toggle for the chat display handler

--- src/main/java/com/mycodefu/werekitten/pipeline/handlers/chat/ChatMessageDisplayHandler.java
+++ src/main/java/com/mycodefu/werekitten/pipeline/handlers/chat/ChatMessageDisplayHandler.java
@@ -4,23 +4,26 @@ import java.awt.Toolkit;
 
 import com.mycodefu.werekitten.event.ChatEventType;
 import com.mycodefu.werekitten.event.Event;
+import com.mycodefu.werekitten.event.KeyboardEventType;
 import com.mycodefu.werekitten.event.UiEventType;
 import com.mycodefu.werekitten.pipeline.PipelineContext;
 import com.mycodefu.werekitten.pipeline.PipelineEvent;
 import com.mycodefu.werekitten.pipeline.events.chat.ChatMessageRecievedEvent;
+import com.mycodefu.werekitten.pipeline.events.keyboard.CKeyPressedEvent;
 import com.mycodefu.werekitten.pipeline.events.ui.UiCreatedEvent;
 import com.mycodefu.werekitten.pipeline.handlers.PipelineHandler;
 
-import javafx.application.Platform;
-import javafx.scene.AccessibleAction;
+import javafx.scene.Group;
 import javafx.scene.control.TextArea;
 
 public class ChatMessageDisplayHandler implements PipelineHandler{
-private TextArea displayArea;
-
+	private TextArea displayArea;
+	private Group group;
+	
 public ChatMessageDisplayHandler() {
-	this.displayArea = new TextArea();
+		this.displayArea = new TextArea();
 	displayArea.setEditable(false);
+	group  = new Group(displayArea);
 }
 
 	@Override
@@ -28,19 +31,26 @@ public ChatMessageDisplayHandler() {
 		return new Event[] {
 				ChatEventType.send,
 				ChatEventType.recieved,
-				UiEventType.UiCreated
+				UiEventType.UiCreated,
+				KeyboardEventType.cPressed
 		};
 	}
 
 	@Override
 	public void handleEvent(PipelineContext context, PipelineEvent event) {
 		if(event instanceof UiCreatedEvent) {
+			
 			displayArea.setTranslateX(0);
 			displayArea.setTranslateY(Toolkit.getDefaultToolkit().getScreenSize().height-200);
-			((UiCreatedEvent)event).getUI().addNode(displayArea);
+			((UiCreatedEvent)event).getUI().addNode(group);
+			
 		}else if(event instanceof ChatMessageRecievedEvent) {
 			ChatMessageRecievedEvent chatEvent = (ChatMessageRecievedEvent)event;
 			displayArea.setText(chatEvent.message+"\n"+displayArea.getText());
+		}else if(event instanceof CKeyPressedEvent) {
+			System.out.println("group.isVisible: "+group.isVisible());
+			group.setVisible(!group.isVisible());
+			System.out.println("group.isVisible: "+group.isVisible());
 		}
 	}
 
