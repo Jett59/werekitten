754af82968f8a9b459dfc755f39f870688d28289
Jett Thompson <jett.thompson.59@gmail.com>
2020-07-07 09:44:35 +1000

    now render the default level in the level designer

--- src/main/java/com/mycodefu/werekitten/level/designer/LevelDesignerUI.java
+++ src/main/java/com/mycodefu/werekitten/level/designer/LevelDesignerUI.java
@@ -1,14 +1,25 @@
 package com.mycodefu.werekitten.level.designer;
 
+import java.util.stream.Collectors;
+
+import com.mycodefu.werekitten.animation.AnimationCompiler;
+import com.mycodefu.werekitten.backgroundObjects.BackgroundObjectBuilder;
+import com.mycodefu.werekitten.level.GameLevel;
+import com.mycodefu.werekitten.level.LevelBuilder;
+import com.mycodefu.werekitten.slide.LayerGroup;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
 public class LevelDesignerUI {
-public static double LEVEL_WIDTH = 1024;
-public static double LEVEL_HEIGHT = 768;
+public static int LEVEL_WIDTH = 1024;
+public static int LEVEL_HEIGHT = 768;
 
 public Scene getScene() {
-	Group group = new Group();
-	return new Scene(group);
+	GameLevel level = new LevelBuilder(new BackgroundObjectBuilder(new AnimationCompiler())).buildLevel("/level.wkl", LEVEL_WIDTH, LEVEL_HEIGHT);
+	Group combined = new Group(level.getLayerGroups().stream()
+	.map(LayerGroup::getGroup)
+	.collect(Collectors.toList()));
+	return new Scene(combined);
 }
 }
