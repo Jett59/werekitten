7cf33063b8972080c96348ac68ab839e9600809b
Luke Thompson <endeavour9@gmail.com>
2020-07-12 17:27:56 +1000

    idea

--- src/main/java/com/mycodefu/werekitten/level/designer/DragHandler.java
+++ src/main/java/com/mycodefu/werekitten/level/designer/DragHandler.java
@@ -1,6 +1,8 @@
 package com.mycodefu.werekitten.level.designer;
 
 import javafx.event.EventHandler;
+import javafx.event.EventTarget;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class DragHandler implements EventHandler<MouseEvent> {
@@ -8,6 +10,16 @@ public class DragHandler implements EventHandler<MouseEvent> {
 	@Override
 	public void handle(MouseEvent event) {
 		System.out.printf("drag detected at x %f and y %f\n", event.getSceneX(), event.getSceneY());
+		EventTarget target = event.getTarget();
+		if (target instanceof ImageView){
+			ImageView imageViewTarget = (ImageView) target;
+			System.out.println("ImageView: " + imageViewTarget);
+			imageViewTarget.setX(event.getX());
+			imageViewTarget.setY(event.getY());
+		} else {
+			System.out.println(target);
+		}
+
 	}
 
 }
