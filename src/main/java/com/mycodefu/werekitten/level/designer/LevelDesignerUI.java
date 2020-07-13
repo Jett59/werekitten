c17abbd192db2360310ac07ab73102d1370bf62b
Jett Thompson <jett.thompson.59@gmail.com>
2020-07-13 19:38:47 +1000

    added an apply button that refreshes the levelGroup from the level data

--- src/main/java/com/mycodefu/werekitten/level/designer/LevelDesignerUI.java
+++ src/main/java/com/mycodefu/werekitten/level/designer/LevelDesignerUI.java
@@ -5,11 +5,10 @@ import com.mycodefu.werekitten.backgroundObjects.BackgroundObjectBuilder;
 import com.mycodefu.werekitten.backgroundObjects.NodeObject;
 import com.mycodefu.werekitten.level.GameLevel;
 import com.mycodefu.werekitten.level.LevelBuilder;
-import com.mycodefu.werekitten.level.data.Element;
 import com.mycodefu.werekitten.slide.LayerGroup;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -23,13 +22,16 @@ public class LevelDesignerUI {
     public static int TOOL_WIDTH = 250;
     public static int LEVEL_HEIGHT = 768;
     private GameLevel level;
+    private Group levelGroup;
+    private Group toolGroup;
 
     private NodeObject selectedNode;
     private Label details;
-
+private Button apply;
+    
     public Scene getScene() {
-        Group levelGroup = buildLevelGroup();
-        Group toolGroup = buildToolGroup();
+        levelGroup = buildLevelGroup();
+        toolGroup = buildToolGroup();
         Group root = new Group(levelGroup, toolGroup);
         createHandlers(toolGroup, levelGroup);
         Scene scene = new Scene(root);
@@ -42,11 +44,21 @@ public class LevelDesignerUI {
         BorderPane border = new BorderPane();
         border.setLayoutX(LEVEL_WIDTH);
         border.setPrefWidth(TOOL_WIDTH);
+        border.setPrefHeight(LEVEL_HEIGHT-100);
         border.setBackground(new Background(new BackgroundFill(Color.CYAN, null, null)));
 
-
+apply = new Button("apply");
+apply.setOnAction(e->{
+	levelGroup.getChildren().clear();
+	GameLevel newLevel = new LevelBuilder(new BackgroundObjectBuilder(new AnimationCompiler())).buildLevel(level.getLevelData(), LEVEL_WIDTH, LEVEL_HEIGHT);
+	level = newLevel;
+	levelGroup.getChildren().addAll(newLevel.getLayerGroups().stream()
+			.map(LayerGroup::getGroup)
+			.collect(Collectors.toList()));
+});
 
         border.setTop(details);
+        border.setBottom(apply);
         return new Group(border);
     }
 
