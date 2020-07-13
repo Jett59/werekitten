01684949bdda015e0bc02b46ad557cd0c5e1c3f6
Jett Thompson <jett.thompson.59@gmail.com>
2020-07-13 17:21:27 +1000

    removed the drag handler

--- src/main/java/com/mycodefu/werekitten/level/designer/LevelDesignerUI.java
+++ src/main/java/com/mycodefu/werekitten/level/designer/LevelDesignerUI.java
@@ -20,7 +20,6 @@ public Scene getScene() {
 	Group combined = new Group(level.getLayerGroups().stream()
 	.map(LayerGroup::getGroup)
 	.collect(Collectors.toList()));
-	combined.setOnDragDetected(DragHandler.detectedHandler);
 	return new Scene(combined);
 }
 }
