d848d34d3efdc12152f2a1769d5a412a73bde9bc
Luke Thompson <endeavour9@gmail.com>
2020-08-03 19:59:14 +1000

    add load for default level

--- src/main/java/com/mycodefu/werekitten/level/LevelReader.java
+++ src/main/java/com/mycodefu/werekitten/level/LevelReader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycodefu.werekitten.files.LocalFileStorage;
 import com.mycodefu.werekitten.level.data.Level;
 
 public class LevelReader {
@@ -11,7 +12,12 @@ public class LevelReader {
 
 	public static Level read(String path) {
 		try {
-			Level level = mapper.readValue(LevelReader.class.getResourceAsStream(path), Level.class);
+			Level level;
+			if (LocalFileStorage.levelFileExists("default level")) {
+				level = mapper.readValue(LocalFileStorage.readLevelFile("default level"), Level.class);
+			} else {
+				level = mapper.readValue(LevelReader.class.getResourceAsStream(path), Level.class);
+			}
 			return level;
 		} catch (IOException e) {
 			e.printStackTrace();
