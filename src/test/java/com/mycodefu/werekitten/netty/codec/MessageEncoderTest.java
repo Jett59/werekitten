226b01ef7297a7304a0dd6712e761cd4a89c290d
Jett Thompson <jett.thompson.59@gmail.com>
2020-08-09 17:33:21 +1000

    added a new unit test for the encoder for if the string is empty

--- src/test/java/com/mycodefu/werekitten/netty/codec/MessageEncoderTest.java
+++ src/test/java/com/mycodefu/werekitten/netty/codec/MessageEncoderTest.java
@@ -15,6 +15,20 @@ import io.netty.buffer.PooledByteBufAllocator;
 
 public class MessageEncoderTest {
 @Test
+void encode_chatMessage_0charTextSize() throws Exception{
+	ChatMessage message = new ChatMessage(System.currentTimeMillis(), "");
+	MessageEncoder encoder = new MessageEncoder();
+	ByteBuf buf = PooledByteBufAllocator.DEFAULT.buffer();
+	encoder.encode(null, message, buf);
+	assertEquals(message.type.getCode(), buf.readByte());
+	assertEquals(message.timeStamp, buf.readLong());
+	byte textSize = buf.readByte();
+	byte[] bytes = new byte[textSize];
+	buf.readBytes(bytes);
+	String decodedString = new String(bytes, StandardCharsets.UTF_8);
+	assertEquals(message.text, decodedString);
+}
+@Test
 void encode_chatMessage_standardMessageSize() throws Exception{
 	ChatMessage message = new ChatMessage(System.currentTimeMillis(), "test message");
 	MessageEncoder encoder = new MessageEncoder();
