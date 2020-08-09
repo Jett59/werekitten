83e844bb4fb24cb1a92ef97d236f20ff5ccdb24e
Jett Thompson <jett.thompson.59@gmail.com>
2020-08-09 20:45:15 +1000

    added a new type of message for if the message uses XSync

--- src/main/java/com/mycodefu/werekitten/network/message/XSyncMessage.java
+++ src/main/java/com/mycodefu/werekitten/network/message/XSyncMessage.java
@@ -1,9 +1,9 @@
 package com.mycodefu.werekitten.network.message;
 
-public class MovementMessage extends Message{
+public class XSyncMessage extends Message{
 public double xSync;
 
-public MovementMessage(MessageType type, long timeStamp, double xSync) {
+public XSyncMessage(MessageType type, long timeStamp, double xSync) {
 	super(type, timeStamp);
 	this.xSync = xSync;
 }
