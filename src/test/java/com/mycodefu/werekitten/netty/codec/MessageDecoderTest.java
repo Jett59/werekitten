0b882d32c99cffc241e1a6caf93516a1e766ba57
Jett Thompson <jett.thompson.59@gmail.com>
2020-08-09 19:30:46 +1000

    added the first unit test for the decoder

---
+++ src/test/java/com/mycodefu/werekitten/netty/codec/MessageDecoderTest.java
@@ -0,0 +1,32 @@
+package com.mycodefu.werekitten.netty.codec;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.mycodefu.werekitten.network.message.ChatMessage;
+import com.mycodefu.werekitten.network.message.MessageType;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
+
+public class MessageDecoderTest {
+@Test
+void decode_chatMessage_0charTextSize() throws Exception{
+	ByteBuf in = PooledByteBufAllocator.DEFAULT.buffer();
+	in.writeByte(MessageType.chat.getCode());
+	long timeStamp = System.currentTimeMillis();
+	in.writeLong(timeStamp);
+	in.writeByte(0);
+	MessageDecoder decoder = new MessageDecoder();
+	List<Object> out = new ArrayList<>();
+	decoder.decode(null, in, out);
+	ChatMessage message = (ChatMessage)out.get(0);
+	assertEquals(MessageType.chat, message.type, "error: types do not match");
+	assertEquals(timeStamp, message.timeStamp, "error: time stamps do not match");
+	assertEquals("", message.text, "error: text does not match");
+}
+}
